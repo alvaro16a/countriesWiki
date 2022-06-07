0f2b8828624dfdf2ff65ffa4e1edd6d4e7207219
Alvaro Mena <alvaro16a@gmail.com>
2022-06-07 09:16:43 -0500

    Se creo el componente search, este se encarga de desplegar la barra de busqueda de paises

---
+++ src/app/components/search/search.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.sass']
+})
+export class SearchComponent implements OnInit {
+  title = 'The Countries Wiki';
+  @Output() newSearchEvent = new EventEmitter<string>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  startNewSearch(value: string) {
+    this.newSearchEvent.emit(value);
+  }
+
+}
